096a30831961d13e206c2d8c718de143fc149ad3
Ronnie Nayak <abhishek.ron.nayak@gmail.com>
2023-08-10 05:53:32 +0530

    add js logic for time

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,15 +1,17 @@
-const sec = document.querySelector(".second-hand");
-const min = document.querySelector(".min-hand");
-const hour = document.querySelector(".hour-hand");
-
-let hour, minute, second;
+const secHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function currentTime() {
   let Time = new Date();
 
-  h = Time.getHours();
-  m = Time.getMinutes();
-  s = Time.getSeconds();
+  let hour = Time.getHours() / 12;
+  let minute = Time.getMinutes() / 60;
+  let second = Time.getSeconds() / 60;
+
+  secHand.style.transform = `rotate(${second * 360 + 90}deg)`;
+  minHand.style.transform = `rotate(${minute * 360 + 90}deg)`;
+  hourHand.style.transform = `rotate(${hour * 360 + 90}deg)`;
 }
 
 setInterval(currentTime, 1000);
